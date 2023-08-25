5672b7b6b5bfe90185330fd197fd92933abdde52
Ekram Hossain <ekramhossain117@gmail.com>
2023-08-25 17:48:17 +0600

    catagory added

--- src/layout/Dashboard/Addmenu/AddMenu.jsx
+++ src/layout/Dashboard/Addmenu/AddMenu.jsx
@@ -24,37 +24,46 @@ const AddMenu = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">Name</label>
-            <input type="text" className="w-full border  border-gray-300 px-3 py-2 rounded" {...register('name', { required: true })} />
+            <input type="text" className="w-full border text-white border-gray-300 px-3 py-2 rounded" {...register('name', { required: true })} />
             {errors.name && <p className="text-red-500 text-xs mt-1">This field is required</p>}
           </div>
           <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">Image URL</label>
-            <input type="text" className="w-full border  border-gray-300 px-3 py-2 rounded" {...register('image', { required: true })} />
+            <input type="text" className="w-full border text-white border-gray-300 px-3 py-2 rounded" {...register('image', { required: true })} />
             {errors.image && <p className="text-red-500 text-xs mt-1">This field is required</p>}
           </div>
           <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">Price</label>
-            <input type="number" step="0.01" className="w-full  border border-gray-300 px-3 py-2 rounded" {...register('price', { required: true })} />
+            <input type="number" step="0.01" className="w-full text-white border border-gray-300 px-3 py-2 rounded" {...register('price', { required: true })} />
             {errors.price && <p className="text-red-500 text-xs mt-1">This field is required</p>}
           </div>
-      
+       
           {/* <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">ID</label>
-            <input type="text" className="w-full border  border-gray-300 px-3 py-2 rounded" {...register('id', { required: true })} />
+            <input type="text" className="w-full border text-white border-gray-300 px-3 py-2 rounded" {...register('id', { required: true })} />
             {errors.id && <p className="text-red-500 text-xs mt-1">This field is required</p>}
           </div> */}
           <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">Category</label>
-            <input type="text" className="w-full text-white border border-gray-300 px-3 py-2 rounded" {...register('category', { required: true })} />
-            {errors.category && <p className="text-red-500 text-xs mt-1">This field is required</p>}
+            <select className="w-full text-white border border-gray-300 px-3 py-2 rounded" {...register('category', { required: true })}>
+              <option value="">Select a category</option>
+              {categoryOptions.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            {errors.category && <p className="text-red-500 text-xs mt-1">Please select a category</p>}
           </div>
           <div className="mb-4">
             <label className="block text-sm font-semibold mb-1">Recipe</label>
-            <textarea className="w-full border  border-gray-300 px-3 py-2 rounded" {...register('recipe', { required: true })} />
+            <textarea className="w-full border text-white border-gray-300 px-3 py-2 rounded" {...register('recipe', { required: true })} />
             {errors.recipe && <p className="text-red-500 text-xs mt-1">This field is required</p>}
           </div>
           <div className="mt-4">
-            <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Add Item</button>
+            <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+              Add Item
+            </button>
           </div>
         </form>
       </div>
