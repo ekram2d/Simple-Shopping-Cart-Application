a831a9fd2471ac4f3310fdb6c4943b8146f6c93e
Ekram Hossain <ekramhossain117@gmail.com>
2023-08-25 19:27:20 +0600

    added

--- src/layout/Dashboard/EditMenu/Editmenu.jsx
+++ src/layout/Dashboard/EditMenu/Editmenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { MenuData } from '../../../CustomHooks/MenuData/MenuData';
 
 const Editmenu = () => {
@@ -9,13 +10,39 @@ const Editmenu = () => {
   // Filter menu data by ID
   const menuItem = menu.find(item => item.id === parseInt(id, 10));
 
-  console.log(menuItem);
+  const { register, handleSubmit } = useForm({ defaultValues: menuItem });
+
+  const onSubmit = (data) => {
+    // Handle the form submission, you can update the menu item with the edited data
+    console.log(data);
+  };
 
   return (
-    <div>
-      {/* Render your edit form or content here */}
-      <h2>Edit Menu Item: {menuItem ? menuItem.name : 'Not Found'}</h2>
-      {/* You can render the details of the menu item here */}
+    <div className="w-full flex justify-center items-center bg-gray-100 h-screen">
+      <div className="w-full bg-white p-6 rounded shadow-md max-w-md">
+        <h2 className="text-2xl font-semibold mb-4 text-center">Edit Menu Item: {menuItem ? menuItem.name : 'Not Found'}</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <label className="block font-semibold">Name</label>
+          <input type="text" className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('name')} />
+
+          <label className="block font-semibold">Image URL</label>
+          <input type="text" className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('image')} />
+
+          <label className="block font-semibold">Price</label>
+          <input type="number" step="0.01" className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('price')} />
+
+          <label className="block font-semibold">Recipe</label>
+          <textarea className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('recipe')} />
+
+          <label className="block font-semibold">ID</label>
+          <input type="text" className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('id')} />
+
+          <label className="block font-semibold">Category</label>
+          <input type="text" className="w-full bg-white border border-gray-300 px-3 py-2 rounded text-black" {...register('category')} />
+
+          <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Save</button>
+        </form>
+      </div>
     </div>
   );
 };
